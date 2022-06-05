151cb70ac69d72abab476dc69d52bf35b164f5a3
Jon Balon <development@jonbalon.com>
2022-06-04 21:07:21 -0400

    Book Ticket Logic

--- main.go
+++ main.go
@@ -4,8 +4,8 @@ import "fmt"
 
 func main() {
 	conferenceName := "Go Conference"
-	conferenceTickets := 50
-	remainingTickets := 50
+	const conferenceTickets int = 50
+	var remainingTickets uint = 50
 
 	fmt.Printf("conferenceTickets is %T, remainingTickets is %T, conferenceName is %T\n", conferenceTickets, remainingTickets, conferenceName)
 
@@ -16,7 +16,7 @@ func main() {
 	var firstName string
 	var lastName string
 	var email string
-	var userTickets int
+	var userTickets uint
 
 	// Ask user for their first name
 	fmt.Println("Enter your first name:")
@@ -34,5 +34,8 @@ func main() {
 	fmt.Println("Enter number of tickets:")
 	fmt.Scan(&userTickets)
 
+	remainingTickets = remainingTickets - userTickets
+
 	fmt.Printf("Thank you %v %v for booking %v tickets. You will received a confirmation email at %v\n", firstName, lastName, userTickets, email)
+	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
 }
